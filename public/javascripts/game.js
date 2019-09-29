f822619b817c96ca1eab18696ad001cd0a4018e5
Paulo H Souza <paulosouza.ti@gmail.com>
2019-09-29 19:53:20 -0300

    desenhos

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -5,7 +5,7 @@ const socket = io('http://localhost:3000')
 let position;
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(1000, 500);
     background(102);
 
     line(width / 2, 0, width / 2, height)
@@ -26,6 +26,12 @@ function draw() {
 socket.on('drawResponse', data => {
     line(data.mouseX, data.mouseY, data.pmouseX, data.pmouseY)
     console.log('Client says:', data)
+})
+
+socket.on('drawIt', data => {
+    console.log('drawId', data);
+
+    document.getElementById('draw').innerHTML = data
 })
 
 socket.on('position', data => {
--- socketApi.js
+++ socketApi.js
@@ -2,32 +2,28 @@ import socketio from 'socket.io';
 
 const io = socketio();
 
-var connections = new Array()
+var connections = [];
 
-let players = [];
-
-const getPlayer = socket => players.filter(p => p.id == socket.id);
+const desenhos = [ 'desenha uma fro', 'desenha um arcoiro', 'desenhe uma bola' ];
 
 io.on('connection', function(socket) {
+
+    // não aceitar mais de duas conexões
+    if (connections.length === 2) return;
+
+
     connections.push(socket.id)
 
+    socket.emit('position', connections.length % 2 == 0 ? 'esquerda' : 'direita');
 
-    players.push(socket);
+    if (connections.length === 2) {
+        console.log(desenhos[parseInt(Math.random() * desenhos.length)])
+        io.emit('drawIt', desenhos[parseInt(Math.random() * desenhos.length)])
+    }
 
-    socket.emit('position', players.length % 2 == 0 ? 'esquerda' : 'direita');
-
-    console.log('join', players.length)
-
-    socket.on('join', () => {
-    })
-
-    socket.on('sendDraw', function(data) { 
-        console.log('Servidor diz:', data)
-
-        socket.broadcast.emit('drawResponse', data)
-      })
+    socket.on('sendDraw', data => socket.broadcast.emit('drawResponse', data))
     
-    console.log('conectou', socket.id)
+    console.log('user connected', socket.id)
 
 })
 
